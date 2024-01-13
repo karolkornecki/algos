62e88b30162d2a48619c8295b0c5aae558c340b1
karolkornecki <karol.kornecki@gmail.com>
2024-01-13 21:05:16 +0100

    W7 | Is UndirectedGraph bipartite

---
+++ algs/graphs/bipartite.py
@@ -0,0 +1,24 @@
+# Is UndirectedGraph bipartite - does the graph is two-colorable ?
+from graphs.undirected_graph import UndirectedGraph
+
+
+class Bipartite:
+    def __init__(self, g: UndirectedGraph):
+        self.g = g
+        self.is_bipartite = True
+        self.color = dict()
+        self.visited = set()
+        for v in g.vertices():
+            if v not in self.visited:
+                self.color[v] = False
+                self.dfs(v)
+
+    def dfs(self, v):
+        self.visited.add(v)
+        for w in self.g.adj(v):
+            if w not in self.visited:
+                self.color[w] = not self.color[v]
+                self.dfs(w)
+            elif self.color[w] == self.color[v]:
+                self.is_bipartite = False
+                return
---
+++ algs/graphs/tests/bipartite_tests.py
@@ -0,0 +1,44 @@
+import unittest
+
+from graphs.bipartite import Bipartite
+from graphs.undirected_graph import UndirectedGraph
+
+
+class BipartiteTestCase(unittest.TestCase):
+
+    def test_should_be_bipartite(self):
+        # given
+        g = UndirectedGraph()
+        g.add_edge(1, 10)
+        g.add_edge(1, 40)
+        g.add_edge(2, 30)
+        g.add_edge(3, 10)
+        g.add_edge(3, 20)
+        g.add_edge(4, 30)
+        g.add_edge(4, 40)
+        g.add_edge(5, 40)
+        # when
+        b = Bipartite(g)
+        # then
+        self.assertTrue(b.is_bipartite)
+
+    def test_should_not_be_bipartite(self):
+        # given
+        g = UndirectedGraph()
+        g.add_edge(1, 10)
+        g.add_edge(1, 40)
+        g.add_edge(2, 30)
+        g.add_edge(3, 10)
+        g.add_edge(3, 20)
+        g.add_edge(4, 30)
+        g.add_edge(4, 40)
+        g.add_edge(5, 40)
+        g.add_edge(5, 4)
+        # when
+        b = Bipartite(g)
+        # then
+        self.assertFalse(b.is_bipartite)
+
+
+if __name__ == '__main__':
+    unittest.main()
