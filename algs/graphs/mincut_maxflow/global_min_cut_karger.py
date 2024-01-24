f23989b10c7e7dc3114fe0c3cc838c2dab21be37
karolkornecki <karol.kornecki@gmail.com>
2024-01-24 17:34:04 +0100

    TODO

---
+++ algs/graphs/mincut_maxflow/global_min_cut_karger.py
@@ -0,0 +1,3 @@
+# Karger's algorithm
+# Works for undirected weighted graph
+# TODO
---
+++ algs/graphs/mincut_maxflow/global_min_cut_stoer_wagner.py
@@ -0,0 +1,7 @@
+# The global minimum cut problem in a graph is a classic problem in graph theory.
+# Given an undirected, weighted graph, a minimum cut is a partition of the vertices into two disjoint sets,
+# such that the sum of the weights of the edges crossing the partition is minimized.
+# The goal is to find the cut with the smallest total weight.
+# One algorithm that is often used to solve the global minimum cut problem is the Stoer-Wagner algorithm.
+# Works for undirected weighted graph
+# TODO
